f13996a8040e406a48fc8e77264f0725d05dd27e
Osman <oosman123@hotmail.com>
2024-06-16 21:18:50 -0400

    indexjs changes

--- frontend/index.js
+++ frontend/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from './App';
 import './index.css';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/',
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
 
-ReactDOM.render(
+const theme = createTheme();
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </ApolloProvider>
 );
